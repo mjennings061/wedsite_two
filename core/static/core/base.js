d4837c48c6393127bbe929a43e285a2e74a7ff21
Michael Jennings <mjennings061@gmail.com>
2021-10-05 21:12:40 +0100

    Removed select.js

--- core/static/core/base.js
+++ core/static/core/base.js
@@ -3,8 +3,3 @@ document.addEventListener('DOMContentLoaded', function() {
     let elems = document.querySelectorAll('select');
     let instances = M.FormSelect.init(elems);
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.scrollspy');
-    var instances = M.ScrollSpy.init(elems, options);
-});
